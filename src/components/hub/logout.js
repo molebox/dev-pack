1b236d76fb7c25da5d79fcb7440d714dc5a10f46
HAINES-DESKTOP\richa <richardkhaines@hotmail.co.uk>
2020-08-14 12:07:23 +0200

    fixed logout

--- src/components/hub/logout.js
+++ src/components/hub/logout.js
@@ -1,14 +1,16 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
 import React from 'react';
-import { UserContext } from './../../context/user-context';
+
 import { navigate } from 'gatsby';
 import OneGraphAuth from 'onegraph-auth';
 import { APP_ID } from '../../butler';
 import Button from './../common/button';
+import { DevCardDispatchContext, DevCardStateContext } from '../../context/devcard-context';
 
 const Logout = () => {
-  const { updateUser } = React.useContext(UserContext);
+  const dispatch = React.useContext(DevCardDispatchContext);
+  const state = React.useContext(DevCardStateContext);
 
   // OneGraphAuth uses the window object to display the popup, we need to check it exists due to SSR.
   const auth =
@@ -18,10 +20,16 @@ const Logout = () => {
         })
       : null;
 
+      React.useEffect(() => {
+        if (!state.isLoggedIn) {
+          navigate('/');
+        }
+      }, [state.isLoggedIn])
+
   const logout = () =>
     auth.logout('github').then((response) => {
       if (response.result === 'success') {
-        updateUser({ isLoggedIn: false });
+        dispatch({ type: 'isLoggedIn', payload: false });
         navigate('/');
         console.log('Logout succeeded');
       } else {
