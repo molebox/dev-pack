34bad3070b25f35f3ee1942c62810f33dc24b4bc
HAINES-DESKTOP\richa <richardkhaines@hotmail.co.uk>
2020-08-14 10:31:15 +0200

    might have it

--- src/components/auth/private-route.js
+++ src/components/auth/private-route.js
@@ -11,11 +11,21 @@ const PrivateRoute = ({ component: Component, location, isLoggedIn, ...rest }) =
   const { loading, error, data } = useQuery(LOGGED_IN_SERVICES);
 
   // check if the user is logged in. If they are already then navigate them to the hub
+  // if (
+  //   data &&
+  //   data.me.serviceMetadata.loggedInServices.length &&
+  //   !data.me.serviceMetadata.loggedInServices[0].isLoggedIn &&
+  //   data.me.serviceMetadata.loggedInServices[0].service === 'GITHUB' &&
+  //   location.pathname !== `/app/login`
+  // ) {
+  //   console.log('not logged in');
+  //   // If we’re not logged in, redirect to the login page.
+  //   navigate(`/app/login`);
+  //   return null;
+  // }
   if (
     data &&
-    data.me.serviceMetadata.loggedInServices.length &&
-    !data.me.serviceMetadata.loggedInServices[0].isLoggedIn &&
-    data.me.serviceMetadata.loggedInServices[0].service === 'GITHUB' &&
+    data.me.serviceMetadata.loggedInServices.length === 0 &&
     location.pathname !== `/app/login`
   ) {
     console.log('not logged in');
